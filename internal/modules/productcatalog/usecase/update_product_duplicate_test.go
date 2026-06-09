b82ba4735528b5b8d4a84f55a77713b37d9078d0
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-06-10 00:49:35 +0800

    commit 120

--- internal/modules/productcatalog/usecase/update_product_duplicate_test.go
+++ internal/modules/productcatalog/usecase/update_product_duplicate_test.go
@@ -6,52 +6,7 @@ import (
 	"time"
 
 	"pos-go/internal/modules/productcatalog/domain"
-	"pos-go/internal/modules/productcatalog/ports"
 )
-
-type fakeUpdateProductRepository struct {
-	product *domain.Product
-}
-
-func (f *fakeUpdateProductRepository) Create(_ context.Context, _ *domain.Product) error {
-	return nil
-}
-
-func (f *fakeUpdateProductRepository) Update(_ context.Context, _ *domain.Product) error {
-	return nil
-}
-
-func (f *fakeUpdateProductRepository) FindByID(
-	_ context.Context,
-	_ string,
-) (*domain.Product, error) {
-	return f.product, nil
-}
-
-type fakeUpdateProductDuplicateChecker struct {
-	updateCalled bool
-	productID    string
-	candidate    ports.ProductDuplicateCandidate
-}
-
-func (f *fakeUpdateProductDuplicateChecker) CheckCreateDuplicate(
-	_ context.Context,
-	_ ports.ProductDuplicateCandidate,
-) error {
-	return nil
-}
-
-func (f *fakeUpdateProductDuplicateChecker) CheckUpdateDuplicate(
-	_ context.Context,
-	productID string,
-	candidate ports.ProductDuplicateCandidate,
-) error {
-	f.updateCalled = true
-	f.productID = productID
-	f.candidate = candidate
-
-	return nil
-}
 
 func TestUpdateProductChecksDuplicateCandidate(t *testing.T) {
 	existing, err := domain.NewProduct(domain.ProductInput{
