437a64adc136d4f66d0cce09e36ba7a8cde7de2c
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-06-10 18:28:45 +0800

    commit 189

--- internal/modules/productcatalog/usecase/list_products.go
+++ internal/modules/productcatalog/usecase/list_products.go
@@ -1,6 +1,10 @@
 package usecase
 
-import "pos-go/internal/modules/productcatalog/ports"
+import (
+	"context"
+
+	"pos-go/internal/modules/productcatalog/ports"
+)
 
 type ListProducts struct {
 	reader ports.ProductReader
@@ -10,4 +14,16 @@ func NewListProducts(reader ports.ProductReader) *ListProducts {
 	return &ListProducts{
 		reader: reader,
 	}
+}
+
+func (uc *ListProducts) Execute(
+	ctx context.Context,
+	_ ListProductsQuery,
+) (ListProductsResult, error) {
+	_, err := uc.reader.List(ctx, ports.ProductListQuery{})
+	if err != nil {
+		return ListProductsResult{}, err
+	}
+
+	return ListProductsResult{}, nil
 }
