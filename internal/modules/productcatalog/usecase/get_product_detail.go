0e79b3f7912ba435a0c39e2143b564b5f6aac697
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-06-10 13:56:36 +0800

    commit 180

--- internal/modules/productcatalog/usecase/get_product_detail.go
+++ internal/modules/productcatalog/usecase/get_product_detail.go
@@ -1,6 +1,10 @@
 package usecase
 
-import "pos-go/internal/modules/productcatalog/ports"
+import (
+	"context"
+
+	"pos-go/internal/modules/productcatalog/ports"
+)
 
 type GetProductDetail struct {
 	reader ports.ProductReader
@@ -10,4 +14,16 @@ func NewGetProductDetail(reader ports.ProductReader) *GetProductDetail {
 	return &GetProductDetail{
 		reader: reader,
 	}
+}
+
+func (uc *GetProductDetail) Execute(
+	ctx context.Context,
+	query GetProductDetailQuery,
+) (GetProductDetailResult, error) {
+	_, err := uc.reader.GetByID(ctx, query.ID)
+	if err != nil {
+		return GetProductDetailResult{}, err
+	}
+
+	return GetProductDetailResult{}, nil
 }
